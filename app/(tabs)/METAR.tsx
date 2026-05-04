1168d85a871975d5656c50063c4458b6c7aabe50
Fadi Al-Atia <fadi.alatia04@gmail.com>
2026-05-04 18:30:13 +0200

    Added Plane Logo mid page

--- app/(tabs)/METAR.tsx
+++ app/(tabs)/METAR.tsx
@@ -7,9 +7,7 @@ export default function METAR() {
   return (
     <SafeAreaView edges={["top"]} className="flex flex-1 bg-white">
       <Header />
-      <View className="flex-1 bg-text-nuetral pl-4 justify-center items-center">
-        <Text className="text-green-500 font-[Mono]">METAR</Text>
-      </View>
+      <View className="flex-1 bg-text-nuetral pl-4 justify-center items-center"></View>
     </SafeAreaView>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,22 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "@/components/header";
+import { Plane } from "lucide-react-native";
 
 export default function index() {
   return (
     <SafeAreaView edges={["top"]} className="flex flex-1 bg-white">
       <Header />
       <View className="flex-1 bg-text-nuetral pl-4 justify-center items-center">
-        <Text className="text-green-500 font-[Mono]">Search</Text>
+        <View className="h-16 w-16 bg-[#F1F5F9] flex justify-center items-center rounded-full">
+          <Plane
+            color={"#1565C0"}
+            fill={"#1565C0"}
+            strokeWidth={1}
+            className="mb-[32px]"
+          ></Plane>
+        </View>
       </View>
     </SafeAreaView>
   );
