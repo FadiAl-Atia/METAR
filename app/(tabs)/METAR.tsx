67a3a8221d9860d3306f34f42f7d5ada2e4ba7c0
Fadi Al-Atia <fadi.alatia04@gmail.com>
2026-05-04 10:27:56 +0200

    Implemented Tabs Layout

---
+++ app/(tabs)/METAR.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function METAR() {
+  return (
+    <View>
+      <Text>METAR</Text>
+    </View>
+  );
+}
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,5 +2,24 @@ import React from "react";
 import { Tabs } from "expo-router";
 
 export default function _layout() {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Search",
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="METAR"
+        options={{
+          title: "METAR",
+        }}
+      ></Tabs.Screen>
+    </Tabs>
+  );
 }
