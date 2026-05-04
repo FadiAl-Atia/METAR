31de2d7fb5e9c457ca1ea17021238cf81c6c18f2
Fadi Al-Atia <fadi.alatia04@gmail.com>
2026-05-04 13:54:43 +0200

    Fixed missing packages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,32 +2,35 @@ import React from "react";
 import { Tabs } from "expo-router";
 import { Search, Cloud } from "lucide-react-native";
 import "../../global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function _layout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Search",
-          tabBarIcon: () => {
-            return <Search></Search>;
-          },
+    <GluestackUIProvider mode="light">
+      <Tabs
+        screenOptions={{
+          headerShown: false,
         }}
-      ></Tabs.Screen>
-      <Tabs.Screen
-        name="METAR"
-        options={{
-          title: "METAR",
-          tabBarIcon: () => {
-            return <Cloud></Cloud>;
-          },
-        }}
-      ></Tabs.Screen>
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Search",
+            tabBarIcon: () => {
+              return <Search></Search>;
+            },
+          }}
+        ></Tabs.Screen>
+        <Tabs.Screen
+          name="METAR"
+          options={{
+            title: "METAR",
+            tabBarIcon: () => {
+              return <Cloud></Cloud>;
+            },
+          }}
+        ></Tabs.Screen>
+      </Tabs>
+    </GluestackUIProvider>
   );
 }
