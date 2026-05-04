70b242587b8c99be77fd7ffe3ae03b219406cadd
Fadi Al-Atia <fadi.alatia04@gmail.com>
2026-05-04 14:09:50 +0200

    Fixed tabs bug

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,16 +3,8 @@ import React from "react";
 
 export default function index() {
   return (
-    <View style={styles.layout}>
-      <Text className="text-red-500">Hi</Text>
+    <View className="flex flex-1 justify-center items-center">
+      <Text className="text-green-500">hello</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  layout: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
