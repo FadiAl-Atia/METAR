14cd9b475758d8090882e3eab804aebe2cd7f7f6
Fadi Al-Atia <fadi.alatia04@gmail.com>
2026-05-04 20:06:53 +0200

    Added initial form logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,8 +6,18 @@ import { Plane, Search } from "lucide-react-native";
 import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import ThemedText from "@/components/themed";
 import { Button, ButtonText } from "@/components/ui/button";
+import { useForm } from "@tanstack/react-form";
 
 export default function index() {
+  const form = useForm({
+    defaultValues: {
+      airport: "KJFK",
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value.airport);
+    },
+  });
+
   return (
     <SafeAreaView edges={["top"]} className="flex flex-1 bg-white">
       <Header />
@@ -15,29 +25,42 @@ export default function index() {
         <View className="h-16 w-16 bg-[#e6ebf0] flex justify-center items-center rounded-full mb-6 ">
           <Plane color={"#1565C0"} fill={"#1565C0"} strokeWidth={1}></Plane>
         </View>
-        <Input
-          variant="rounded"
-          size="md"
-          isRequired={true}
-          className={`w-3/4 border-[#94A3B8] mb-6`}
-        >
-          <InputSlot className="pl-3">
-            <InputIcon as={Search} color="#94A3B8"></InputIcon>
-          </InputSlot>
-          <InputField
-            placeholder="Enter the airport ICAO"
-            maxLength={4}
-            placeholderTextColor={"#94A3B8"}
-            autoCapitalize="characters"
-          ></InputField>
-        </Input>
+        <form.Field
+          name="airport"
+          children={(field) => {
+            return (
+              <Input
+                variant="rounded"
+                size="md"
+                isRequired={true}
+                className={`w-3/4 border-[#94A3B8] mb-6`}
+              >
+                <InputSlot className="pl-3">
+                  <InputIcon as={Search} color="#94A3B8"></InputIcon>
+                </InputSlot>
+                <InputField
+                  placeholder="Enter the airport ICAO"
+                  maxLength={4}
+                  placeholderTextColor={"#94A3B8"}
+                  autoCapitalize="characters"
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  selectTextOnFocus
+                  onChange={(e) => field.handleChange(e.nativeEvent.text)}
+                ></InputField>
+              </Input>
+            );
+          }}
+        ></form.Field>
+
         <Button
           variant="solid"
           size="md"
           action="primary"
           className="bg-brand mb-3"
+          onPress={form.handleSubmit}
         >
-          <ButtonText>Click me</ButtonText>
+          <ButtonText>Submit</ButtonText>
         </Button>
         <ThemedText styles="text-[#94A3B8] text-sm text-center">
           Quickly acces METAR data for any airport around the planet.
