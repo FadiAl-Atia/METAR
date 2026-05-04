71c36540152bf59ecaf4ecc9e609ddb8d5042326
Fadi Al-Atia <fadi.alatia04@gmail.com>
2026-05-04 19:16:49 +0200

    Added Input and Buttons, Returned Rules as they affect the button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,42 @@
 import { View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "@/components/header";
-import { Plane } from "lucide-react-native";
+import { Plane, Search } from "lucide-react-native";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
+import ThemedText from "@/components/themed";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function index() {
   return (
     <SafeAreaView edges={["top"]} className="flex flex-1 bg-white">
       <Header />
       <View className="flex-1 bg-text-nuetral pl-4 justify-center items-center">
-        <View className="h-16 w-16 bg-[#F1F5F9] flex justify-center items-center rounded-full">
-          <Plane
-            color={"#1565C0"}
-            fill={"#1565C0"}
-            strokeWidth={1}
-            className="mb-[32px]"
-          ></Plane>
+        <View className="h-16 w-16 bg-[#e6ebf0] flex justify-center items-center rounded-full mb-6 ">
+          <Plane color={"#1565C0"} fill={"#1565C0"} strokeWidth={1}></Plane>
         </View>
+        <Input
+          variant="rounded"
+          size="md"
+          isRequired={true}
+          className={`w-3/4 border-[#94A3B8] mb-6`}
+        >
+          <InputSlot className="pl-3">
+            <InputIcon as={Search} color="#94A3B8"></InputIcon>
+          </InputSlot>
+          <InputField
+            placeholder="Enter the airport ICAO"
+            maxLength={4}
+            placeholderTextColor={"#94A3B8"}
+            autoCapitalize="characters"
+          ></InputField>
+        </Input>
+        <Button variant="solid" size="md" action="negative" className="">
+          <ButtonText>Click me</ButtonText>
+        </Button>
+        <ThemedText styles="text-[#94A3B8] text-sm text-center">
+          Quickly acces METAR data for any airport around the planet.
+        </ThemedText>
       </View>
     </SafeAreaView>
   );
