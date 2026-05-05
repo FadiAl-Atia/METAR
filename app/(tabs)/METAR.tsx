c357a80ec2d9ac754af07ae6d939f0eb945083c7
Fadi Al-Atia <fadi.alatia04@gmail.com>
2026-05-05 13:01:13 +0200

    Added Icons

--- app/(tabs)/METAR.tsx
+++ app/(tabs)/METAR.tsx
@@ -3,13 +3,22 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "@/components/header";
 import { useLocalSearchParams } from "expo-router";
+import InfoCard from "@/components/info-card";
 
 export default function METAR() {
   const { metar } = useLocalSearchParams();
   return (
     <SafeAreaView edges={["top"]} className="flex flex-1 bg-white">
       <Header />
-      <View className="flex-1 bg-text-nuetral justify-center items-center"></View>
+      <View className="flex-1 bg-text-nuetral justify-center items-center">
+        <InfoCard
+          title="TEMPERATURE & DEWPOINT"
+          r1_title="Humidity"
+          r1_value={68}
+          r2_title="QNH (Altimeter)"
+          r2_value={1013}
+        ></InfoCard>
+      </View>
     </SafeAreaView>
   );
 }
